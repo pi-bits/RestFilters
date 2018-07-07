bcae0f32ff2c9c736e4684ea47f51288ffc24743
prashantnaik <prashantnaik@outlook.com>
2018-07-07 13:40:02 +0100

    Initial Commit

---
+++ src/main/java/com/serviceinfotech/filters/RestFilters/RestFiltersApplication.java
@@ -0,0 +1,12 @@
+package com.serviceinfotech.filters.RestFilters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestFiltersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestFiltersApplication.class, args);
+	}
+}
